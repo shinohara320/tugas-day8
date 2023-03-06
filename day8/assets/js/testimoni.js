3c455f5e42a6488a977fb7fd13b4a6a83c969fa1
shinohara320 <daudmanuwu@gmail.com>
2023-03-06 10:38:57 +0900

    update testimonials

--- day8/assets/js/testimoni.js
+++ day8/assets/js/testimoni.js
@@ -43,11 +43,11 @@ class CompanyTestimonial extends Testimonial {
     }
 }
 
-const testimonial1 = new AuthorTestimonial("Surya Elidanto", "Mantap, keren banget jasanya", "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
+const testimonial1 = new AuthorTestimonial("Mas hiro", "Mantap", "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
 
-const testimonial2 = new AuthorTestimonial("Surya Ganteng", "oke sih, oke lah", "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
+const testimonial2 = new AuthorTestimonial("Bu Mulyani", "Oke, Sip!", "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
 
-const testimonial3 = new CompanyTestimonial("Apple", "Gege gaming!", "https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")
+const testimonial3 = new CompanyTestimonial("A kang kung", "mantepp!", "https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")
 
 let testimonialData = [testimonial1, testimonial2, testimonial3]
 let testimonialHTML = "";
